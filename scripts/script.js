c0732a37cd6dc605d159f9153a93613bce1bdc8b
JNeo77 <lifted_2000@yahoo.com>
2024-06-14 19:04:02 -0400

    Write logic to get a computer choice
    Create a function that will return a random computer choice using
    Math.random.

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function getComputerChoice () {
+  const randomNum = Math.floor(Math.random() * 3);
+  switch (randomNum) {
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+      return 'scissors';
+      break;   
+  }
+}
